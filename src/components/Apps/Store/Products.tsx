4e74cd654a2581291b222868bcb489d8616e0aac
Vivek Kumar <kumar97vivek@gmail.com>
2024-04-10 11:00:04 +0530

    Add Store nav

--- src/components/Apps/Store/Products.tsx
+++ src/components/Apps/Store/Products.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Product from './utils/Product';
+import StoreNav from './StoreNav';
 
 const Store: React.FC = () => {
 	const [products, setProducts] = useState<Product[]>([]);
@@ -12,27 +13,30 @@ const Store: React.FC = () => {
 	}, []);
 
 	return (
-		<div className="bg-white">
-			<div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-				<h2 className="sr-only">Products</h2>
+		<>
+			<StoreNav />
+			<div className="bg-white">
+				<div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+					<h2 className="sr-only">Products</h2>
 
-				<div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-					{products.map((product) => (
-						<a key={product.id} href={`/product/` + product.id} className="group">
-							<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7 h-32">
-								<img
-									src={product.image}
-									alt={product.title}
-									className="object-cover object-center group-hover:opacity-75"
-								/>
-							</div>
-							<h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
-							<p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
-						</a>
-					))}
+					<div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+						{products.map((product) => (
+							<a key={product.id} href={`/product/` + product.id} className="group">
+								<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7 h-32">
+									<img
+										src={product.image}
+										alt={product.title}
+										className="object-cover object-center group-hover:opacity-75"
+									/>
+								</div>
+								<h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
+								<p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+							</a>
+						))}
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
---
+++ src/components/Apps/Store/StoreNav.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import { Popover } from '@headlessui/react'
+import { ShoppingBagIcon } from '@heroicons/react/24/outline'
+
+const StoreNav: React.FC = () => {
+	const [productCategories, setProductCategories] = useState([]);
+	const PRODUCT_URL = 'https://fakestoreapi.com/products/categories';
+
+	useEffect(() => {
+		fetch(PRODUCT_URL)
+			.then((res) => res.json())
+			.then((data) => setProductCategories(data))
+			.catch((error) => console.error('Error fetching product:', error));
+	}, []);
+
+	return (
+		<div className="bg-white">
+			<header className="relative bg-white">
+				<nav aria-label="Top" className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+					<div className="border-b border-gray-200 px-4 pb-14 sm:px-0 sm:pb-0">
+						<div className="flex h-16 items-center justify-between">
+
+							<div className="absolute inset-x-0 bottom-0 sm:static sm:flex-1 sm:self-stretch">
+								<div className="flex h-14 space-x-8 overflow-x-auto border-t px-4 pb-px sm:h-full sm:justify-center sm:overflow-visible sm:border-t-0 sm:pb-0">
+									{productCategories.map((item) => (
+										<a
+											key={item}
+											href={item}
+											className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800 capitalize"
+										>
+											{item}
+										</a>
+									))}
+								</div>
+							</div>
+
+							<div className="flex flex-1 items-center justify-end">
+								<div className="ml-4 flow-root lg:ml-8">
+									<a href="#" className="group -m-2 flex items-center p-2">
+										<ShoppingBagIcon
+											className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+											aria-hidden="true"
+										/>
+										<span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
+										<span className="sr-only">items in cart, view bag</span>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</nav>
+			</header>
+		</div>
+	)
+}
+export default StoreNav;
