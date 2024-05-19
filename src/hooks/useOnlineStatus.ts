fd14de144aa49b66d0d4abcac565e15d61fdc874
Vivek Kumar <kumar97vivek@gmail.com>
2024-05-20 02:00:58 +0530

    Add swal error on offrline

--- src/hooks/useOnlineStatus.ts
+++ src/hooks/useOnlineStatus.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 const useOnlineStatus = () => {
 
@@ -7,6 +8,23 @@ const useOnlineStatus = () => {
 	useEffect(() => {
 		window.addEventListener("offline", () => {
 			setOnlineStatus(false);
+
+			const Toast = Swal.mixin({
+				toast: true,
+				position: "top-end",
+				showConfirmButton: false,
+				timer: 5000,
+				timerProgressBar: true,
+				didOpen: (toast) => {
+					toast.onmouseenter = Swal.stopTimer;
+					toast.onmouseleave = Swal.resumeTimer;
+				}
+			});
+
+			Toast.fire({
+				icon: "error",
+				title: "Seems you are offline. Please check your internet connection!",
+			});
 		});
 
 		window.addEventListener("online", () => {
